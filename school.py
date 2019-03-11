eefc1e17af62faa21958f90d72a15d67441d754e
jsavory-94 <james.pa.savory@gmail.com>
2019-03-11 07:50:41 -0400

    done

--- school.py
+++ school.py
@@ -1,31 +1,21 @@
 from collections import defaultdict 
 
 class School:
-    def __init__(self, name, student_name=None, student_grade=None):
+    def __init__(self, name):
         self.name = name
-        self.student_name = student_name
-        self.student_grade = student_grade
-    
-    roster_dict = {}
+        self._roster = {}  
 
     def roster(self):
-        return self.roster_dict
+        return self._roster
 
-    def add_student(self,s_name,s_grade):
-        self.student_name = str(s_name)
-        self.student_grade = s_grade
-        
-        student_list = []
-        student_tuple = (self.student_grade, self.student_name)
-        student_list.append(student_tuple)
-        student_dict = defaultdict(list)
-
-        for self.student_grade, self.student_name in student_list:
-            student_dict[self.student_grade].append(self.student_name)
+    def add_student(self, name, grade):
+        if grade in self._roster:
+            self._roster[grade].append(name)
+        else:
+            self._roster[grade] = [name]
+        return self._roster
     
-        self.roster_dict.update(student_dict)
-    
-    def grade(self, key):
-        return self.roster_dict[key]
+    def grade(self, grade):
+        return self._roster[grade]
 
         
